2cc857a55436200582879144fc2abfb7bd1c680a
Ruben Aguilar <ragui56@wgu.edu>
2026-06-01 13:33:37 -0700

    add post, mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,25 @@
 const express = require('express')
+const MongoClient = require('mongodb').MongoClient
+
+
 const app = express()
+
+const connectionString = `mongodb+srv://user:password@cluster0.uvc26cg.mongodb.net/`
+
+MongoClient.connect(connectionString)
+  .then(console.log('Connected to Database'))
+  .catch(err => console.err(err))
+
+
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
+app.post('/quotes', (req, res) => {
+  console.log(req.body)
+})
 
 
 
