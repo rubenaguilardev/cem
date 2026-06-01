b3a6e5b4fc6594e3d86699ab671e402b36851841
Ruben Aguilar <ragui56@wgu.edu>
2026-06-01 14:43:10 -0700

    add collection

--- backend/server.js
+++ backend/server.js
@@ -3,32 +3,38 @@ const MongoClient = require('mongodb').MongoClient
 
 
 const app = express()
-
-const connectionString = `mongodb+srv://user:password@cluster0.uvc26cg.mongodb.net/`
-
-MongoClient.connect(connectionString)
-  .then(console.log('Connected to Database'))
-  .catch(err => console.err(err))
-
-
+const PORT = 3001
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-})
+const connectionString = `mongodb+srv://@cluster0.uvc26cg.mongodb.net/`
 
-app.post('/quotes', (req, res) => {
-  console.log(req.body)
-})
+MongoClient.connect(connectionString)
+  .then(client => {
+    const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
+
+    app.get('/', (req, res) => {
+      quotesCollection
+        .find()
+        .toArray()
+        .then(results => {
+          console.log(results)
+          res.sendFile(__dirname + '/index.html')
+        })
+        .catch(error => console.error(error))
+    })
+
+    app.post('/quotes', (req, res) => {
+      quotesCollection
+        .insertOne(req.body)
+        .then(result => {
+          res.redirect('/')
+        })
+        .catch(error => console.error(error))
+    })
+
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  })
+  .catch(error => console.error(error))
 
 
-
-
-
-
-
-
-
-
-const PORT = 3001
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
